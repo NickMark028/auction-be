dd59249eb7cf8b10666440c73789aaa5cac03bcd
Nick Mark <nickmark028@gmail.com>
2021-12-23 15:36:22 +0700

    Update error message

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,4 +1,4 @@
-import e, { Request, Response, Router } from "express";
+import { Request, Response, Router } from "express";
 import db from "../utils/db";
 
 const categoryRouter = Router();
@@ -20,12 +20,12 @@ categoryRouter.get('/', async (req: Request, res: Response) => {
 categoryRouter.get('/:path', async (req: Request, res: Response) => {
     try {
         const path = req.params.path;
-        const result = await db('QueryProductView').where({ path }).select();
+        const result = await db('QueryProductView').where({ categoryPath: path }).select();
         res.status(200).json(result);
     }
     catch (error) {
         res.status(500).json({
-            error: e.toString()
+            error: 'Server error'
         });
     }
 })
