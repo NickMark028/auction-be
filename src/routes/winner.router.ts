511c6c4ff47b20fe1d8e4aef9b75e8b91c15f77b
thanhvu2901 <vcthanh2901@gmail.com>
2022-01-05 15:23:25 +0700

    get email

--- src/routes/winner.router.ts
+++ src/routes/winner.router.ts
@@ -6,9 +6,11 @@ const winnerRouter = express.Router();
 
 winnerRouter.get('/winner-email', async (req, res) => {
   try {
-    const rawquery = `SELECT U.email FROM auction.queryproductdetailview B, user U where B.topBidderId = U.id and B.timeExpired < (select now())`;
+    const rawquery = `SELECT U.email,B.name, B.currentPrice,b.topbidder,B.topbidderId FROM auction.queryproductdetailview B, user U where B.topBidderId = U.id and B.timeExpired < (select now())`;
     const [rows, fields] = await db.raw(rawquery);
     res.send(rows).status(201);
+
+    //tự động gửi mail xác nhận
   } catch (err) {
     return res.status(401).json({ error: err });
   }
