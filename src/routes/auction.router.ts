034803fce4773b2eae2d5fa21df96da233dd119e
Nick Mark <nickmark028@gmail.com>
2022-01-13 23:35:33 +0700

    Fix status code

--- src/routes/auction.router.ts
+++ src/routes/auction.router.ts
@@ -1,3 +1,4 @@
+import { log } from 'console';
 import express, { Request, Response } from 'express';
 import auctionModel from '../models/auction.model';
 
@@ -14,16 +15,16 @@ auctionRouter.post('/', async (req, res) => {
 
     res.status(201).json(auction);
   } catch (error) {
-    res.send(error).status(401);
+    res.status(400).send(error);
   }
 });
 
 auctionRouter.get('/:id', async (req, res) => {
   try {
     const auctionLog = await auctionModel.bidHistory(req.params.id);
-    res.send(auctionLog).status(201);
+    res.status(200).send(auctionLog);
   } catch (err) {
-    return res.status(401).json({ error: err });
+    return res.status(400).json({ error: err });
   }
 });
 
